ccf46c34e737d64fa9acf49c03df1976088d10c5
Raymond Yee <raymond.yee@gmail.com>
2012-11-21 07:26:52 -0800

    Turn the debugging off!

--- lib/thrift/transport/THttpClient.py
+++ lib/thrift/transport/THttpClient.py
@@ -94,7 +94,7 @@ class THttpClient(TTransportBase):
     def open(self):
         protocol = httplib.HTTP if self.scheme == 'http' else httplib.HTTPS
         self.__http = protocol(self.endpoint_host, self.endpoint_port)
-        self.__http.set_debuglevel(1) #TODO
+        self.__http.set_debuglevel(0) #TODO
 
     def close(self):
         self.__http.close()
